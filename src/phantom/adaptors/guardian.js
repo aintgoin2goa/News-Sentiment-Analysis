1e23282c8e7a8f8d1697f804634ef2434c2d0c7c
Paul Wilson <paul.wilson@akqa.com>
2014-08-12 17:05:12 +0100

    Adding extract method to guardian adaptor

--- src/phantom/adaptors/guardian.js
+++ src/phantom/adaptors/guardian.js
@@ -11,3 +11,39 @@ exports.crawl = {
         return urls;
     }
 };
+
+exports.extract = {
+    execute : function(){
+        var extracted = {
+            title : '',
+            date : null,
+            content : ''
+        };
+        var error = function(txt){
+            return {
+                result : 'error',
+                message : text
+            }
+        };
+        var titleEl = document.querySelector('h1.content__headline');
+        var dateEl = document.querySelector('content__dateline time');
+        var contentEl =  document.querySelector('.js-article__body');
+        if(!titleEl){
+            return error('could not find title');
+        }
+
+        if(!dateEl || dateEl.dataset.timestamp){
+            return error('could not find date');
+        }
+
+        if(!contentEl || !contentEl.textContent){
+            return error('could not find content');
+        }
+
+        extracted.title = titleEl.textContent;
+        extracted.date = dateEl.dataset.timestamp;
+        extracted.content = contentEl.textContent;
+
+        return extracted;
+    }
+};
